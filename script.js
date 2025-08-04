ab27f97fb4c4775181a415bd314c2765bb4ab55a
sdahal-dev <sdahal.contact@gmail.com>
2025-08-03 23:56:39 -0500

    Add On-Scroll Animations using Intersection Observer

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
+// Menu UI and Menu-Close Configuration
 const menuBtn = document.querySelector(".ri-menu-line");
 const menuScreen = document.querySelector(".menu-screen");
+const closeBtn = document.querySelector(".ri-close-line");
 
 let menuClicked = false;
 
 menuBtn.addEventListener("click", (event) => {
   menuScreen.classList.toggle("hidden");
   menuClicked = !menuClicked;
+});
+
+closeBtn.addEventListener("click", (event) => {
+  menuScreen.classList.toggle("hidden");
+  menuClicked = !menuClicked;
+});
+
+// Animation OnScroll Configuration
+const products = document.querySelectorAll(
+  ".first-product, .second-product, .third-product, .our-img"
+);
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("slide-in");
+    } else {
+      entry.target.classList.remove("slide-in");
+    }
+  });
+});
+
+products.forEach((product) => {
+  observer.observe(product);
 });
