e2f2dc3e42b58d2cf334a5bde91e297dd7d3dc77
sdahal-dev <sdahal.contact@gmail.com>
2025-08-03 19:54:20 -0500

    Fixed minor UI issues for desktop

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector(".ri-menu-line");
 const menuScreen = document.querySelector(".menu-screen");
 
 let menuClicked = false;
+
 menuBtn.addEventListener("click", (event) => {
   menuScreen.classList.toggle("hidden");
   menuClicked = !menuClicked;
